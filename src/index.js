f612c817aac51013081acd290b15cea45bff5394
Stephano <stephanochang21@gmail.com>
2022-09-18 01:11:08 -0500

    Fisrt Version

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import express from "express";  //NO me funciona pero si esta bien
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
+
+
+import indexRoute from './routes/index.js'
+
+const app = express();
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+app.set('views', join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.use(indexRoute)
+
+app.use(express.static(join(__dirname,'public')))
+app.listen(3000, () =>
+  console.log('Example app listening on port 3000!'),
+
+);
